9ebdfab3609bbf7f5aca5afcb792cc7796069b32
Diamondh3art <165077238+Diamondh3art@users.noreply.github.com>
2025-09-09 12:25:09 +0200

    Initial commit (Spring Initializr)

---
+++ src/main/java/com/example/transferprojekt/TransferprojektApplication.java
@@ -0,0 +1,13 @@
+package com.example.transferprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransferprojektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransferprojektApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/transferprojekt/TransferprojektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.transferprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransferprojektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
