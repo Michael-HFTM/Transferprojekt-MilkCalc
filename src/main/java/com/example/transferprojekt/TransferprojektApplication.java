7d58d2c201b58f7e98a9042ed4890a0f4c3182d7
Diamondh3art <165077238+Diamondh3art@users.noreply.github.com>
2025-09-12 18:47:36 +0200

    initial testing for the dataclasses

--- src/main/java/com/example/transferprojekt/TransferprojektApplication.java
+++ src/main/java/com/example/transferprojekt/TransferprojektApplication.java
@@ -1,13 +1,26 @@
 package com.example.transferprojekt;
 
+import com.example.transferprojekt.dataclasses.Address;
+import com.example.transferprojekt.dataclasses.Supplier;
+import com.example.transferprojekt.dataclasses.SupplierNumber;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TransferprojektApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TransferprojektApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        //SpringApplication.run(TransferprojektApplication.class, args);
+
+        System.out.println("TransferprojektApplication started");
+
+        Supplier sup = new Supplier("test@mail.com", new Address("9999", "Musterweg 1", "Gasser", "Michael", "Musterdorf"), new SupplierNumber(1));
+
+        System.out.println(sup);
+        System.out.println(" ");
+        System.out.println(sup.getAddress().toString());
+
+    }
 
 }
