9b1a2a40bb5b3c408198946b60e02b8b0a40f2f4
Diamondh3art <165077238+Diamondh3art@users.noreply.github.com>
2025-09-16 11:30:50 +0200

    Fixed Address dataclass

--- src/main/java/com/example/transferprojekt/dataclasses/Address.java
+++ src/main/java/com/example/transferprojekt/dataclasses/Address.java
@@ -2,13 +2,12 @@ package com.example.transferprojekt.dataclasses;
 
 public class Address {
 
-    private String city, firstName, lastName, street, zip;
+    private String name, street, city, zip;
 
-    public Address(String zip, String street, String lastName, String firstName, String city) {
+    public Address(String name, String street, String city, String zip) {
         this.zip = zip;
         this.street = street;
-        this.lastName = lastName;
-        this.firstName = firstName;
+        this.name = name;
         this.city = city;
     }
 
@@ -20,21 +19,14 @@ public class Address {
         this.city = city;
     }
 
-    public String getFirstName() {
-        return firstName;
+    public String getName() {
+        return name;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
+    public void setFirstName(String name) {
+        this.name = name;
     }
 
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
 
     public String getStreet() {
         return street;
@@ -55,10 +47,9 @@ public class Address {
     @Override
     public String toString() {
         return "Address{" +
-                "city='" + city + '\'' +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
+                "name='" + name + '\'' +
                 ", street='" + street + '\'' +
+                ", city='" + city + '\'' +
                 ", zip='" + zip + '\'' +
                 '}';
     }
